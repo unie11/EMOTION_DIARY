190e014b682a548a5d240b480d09557a03140d28
yunseo <choijang1117@hanmail.net>
2024-02-05 22:51:01 +0900

    페이지 라우팅 1 - React Router 기본

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import './App.css';
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import Home from "./pages/Home";
+import Edit from "./pages/Edit";
+import New from "./pages/New";
+import Diary from "./pages/Diary";
+import RouteTest from "./components/RouteTest";
+
+function App() {
+  return (
+      <BrowserRouter>
+          <div className="App">
+              <h2>App.js</h2>
+              <Routes>
+                  <Route path="/" element={<Home />} />
+                  <Route path="/new" element={<New />} />
+                  <Route path="/edit" element={<Edit />} />
+                  <Route path="/diary" element={<Diary />} />
+              </Routes>
+              <RouteTest />
+          </div>
+      </BrowserRouter>
+  );
+}
+
+export default App;
