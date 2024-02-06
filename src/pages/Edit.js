aae3b263b79946974c1f163f17358f8c96bb3559
최윤서 <yunseo@entropykorea.com>
2024-02-06 18:54:08 +0900

    페이징 라우팅 2 - React Router 응용

--- src/pages/Edit.js
+++ src/pages/Edit.js
@@ -1,8 +1,23 @@
+import {useSearchParams, useNavigate} from "react-router-dom";
+
 const Edit = () => {
+    const navigate = useNavigate();
+    const [searchParams, setSearchParams] = useSearchParams();
+    const id = searchParams.get('id');
+    const mode = searchParams.get('mode');
+    console.log(mode);
+    console.log(id);
+
     return (
         <div>
             <h1>Edit</h1>
             <p>이곳은 일기 수정페이지 입니다.</p>
+            <button onClick={() => setSearchParams({who:'yunseo'})}>QS바꾸기</button>
+            <button onClick={() => {
+                navigate("/home");
+            }}>HOME으로가기</button>
+            <button onClick={() => {
+            navigate(-1);}}>뒤로가기</button>
         </div>
     );
 };
