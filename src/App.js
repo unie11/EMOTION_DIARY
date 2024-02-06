e49126163c31c22b754e107b489947f3b90a7b08
최윤서 <yunseo@entropykorea.com>
2024-02-06 19:41:27 +0900

    프로젝트 기초 공사 1

--- src/App.js
+++ src/App.js
@@ -4,20 +4,27 @@ import Home from "./pages/Home";
 import Edit from "./pages/Edit";
 import New from "./pages/New";
 import Diary from "./pages/Diary";
-import RouteTest from "./components/RouteTest";
+import MyButton from "./components/MyButton"
+import MyHeader from "./components/MyHeader"
 
 function App() {
   return (
       <BrowserRouter>
           <div className="App">
+              <MyHeader headText={'App'}
+                        leftChild={<MyButton text={'왼쪽버튼'} onClick={() => alert("왼쪽 클릭")} />}
+                        rightChild={<MyButton text={'오른쪽버튼'} onClick={() => alert("오른쪽 클릭")} />}
+              />
               <h2>App.js</h2>
+              <MyButton text={"버튼"} onClick={() => alert("버튼 클릭")} type={"positive"} />
+              <MyButton text={"버튼"} onClick={() => alert("버튼 클릭")} type={"negative"} />
+              <MyButton text={"버튼"} onClick={() => alert("버튼 클릭")} s/>
               <Routes>
                   <Route path="/" element={<Home />} />
                   <Route path="/new" element={<New />} />
                   <Route path="/edit" element={<Edit />} />
                   <Route path="/diary/:id" element={<Diary />} />
               </Routes>
-              <RouteTest />
           </div>
       </BrowserRouter>
   );
